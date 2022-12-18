99c2e464e140395a0367911fc4a9864a80af82a7
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2022-12-18 13:12:46 +0200

    added endpoints

--- backend/model/products.js
+++ backend/model/products.js
@@ -5,7 +5,11 @@ const productSchema = new Schema({
     ssid: { type: String },
     type: { type: String, enum: ['SUMMER_OUTFIT', 'WINTER_OUTFIT', 'HAND_TOOLS', 'POWER_TOOLS', 'GROCERIES'] },
     name: { type: String },
-    price: { type: Number }
+    price: { type: Number },
+    review:{
+        score:{type:Number,min:0,max:5},
+        comment:{type:String}
+    }
 }
 );
 const Product = mongoose.model('Product', productSchema,'Products');
--- backend/routes.js
+++ backend/routes.js
@@ -4,8 +4,8 @@ import Product from "./model/products.js";
 import Complaint from "./model/complaints.js"
 
 const router = express.Router()
-
-router.get("/products",async (req,res)=>{
+//landing page gets all products
+router.get("/products", async (req, res) => {
     try {
         const records = await Product.find();
         res.status(200).json(records);
@@ -13,7 +13,8 @@ router.get("/products",async (req,res)=>{
         res.status(400).json({ message: e.message });
     }
 });
-router.get("/complaints",async (req,res)=>{
+//landing page for customer-service gets all complaints 
+router.get("/complaints", async (req, res) => {
     try {
         const records = await Complaint.find();
         res.status(200).json(records);
@@ -21,15 +22,61 @@ router.get("/complaints",async (req,res)=>{
         res.status(400).json({ message: e.message });
     }
 });
-
-router.post("/porduct",async (req,res)=>{
-
-    var product = req.body
-    await Product.insertMany(product)
-    res.json(product)
+//method used for product seller to create new project
+router.post("/porduct", async (req, res) => {
+    try {
+        var product = req.body
+        await Product.insertMany(product)
+        res.json(product)
+    } catch (e) {
+        res.status(400).json({ message: e.message });
+    }
 });
-router.post("/complain",async (req,res)=>{
-    var complaint = req.body
-    await Complaint.insertMany(complaint)
-    res.json(complaint)
+//complaint sending for customers
+router.post("/complain", async (req, res) => {
+    try {
+        var complaint = req.body
+        await Complaint.insertMany(complaint)
+        res.json(complaint)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
 });
+//reviewing products by users
+router.patch("/rewview", async (req, res) => {
+    try {
+
+        var{ssid,comment,score}=req.body
+
+        var review = await Product.findOneAndUpdate({ ssid: ssid }, { review:{score,comment } }, { new: true });
+        res.json(review);
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+});
+//reply to complaint by customer service
+router.patch("/complain", async (req, res) => {
+    try {
+        var { response, email } = req.body
+        var complaint = await Complaint.findByIdAndUpdate({ email: email }, { response: response }, { new: true })
+        res.json(complaint)
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+
+});
+//removing a product by seller
+router.delete("/:ssid", async (req, res) => {
+    try {
+        var ssid = req.params
+        var product = await Product.findOneAndDelete({ ssid: ssid });
+        res.json(product);
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+
+})
