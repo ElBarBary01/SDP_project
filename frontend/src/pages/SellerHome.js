6fddacdcd15cef3e42543978d9eb371d189ee7a9
sbayce <youssef2311@gmail.com>
2022-12-22 18:07:46 +0200

    fixed front-endd

--- frontend/src/pages/SellerHome.js
+++ frontend/src/pages/SellerHome.js
@@ -2,33 +2,49 @@ import react, { useState } from "react";
 import NavbarSeller from "../NavbarSeller";
 
 function SellerHome(){
-    const[note, setNote] = useState({
-        title: "",
-        content: ""
-    });
-    function handleChange(event){
-        const {name, value} = event.target;
-        setNote({title: name, content: value})
+    const [outfitType, setOutfitType] = useState(false);
+    const [groceriesType, setGroceriesType] = useState(false);
+    const [toolType, setToolType] = useState(false);
+    
+    const [productName, setProductName] = useState("");
+    const [productPrice, setProductPrice] = useState("");
+    const [productCategory, setProductCategory] = useState("");
+    const [productType, setProductType] = useState("");
+
+    function handleName(e){
+        var input = e.target.value;
+        setProductName(input);
+    }
+    function handlePrice(e){
+        var input = e.target.value;
+        setProductPrice(input);
+    }
+    function handleCategory(e){
+        var input = e.target.value;
+        setProductCategory(input);
+    }
+    function handleType(e){
+        var input = e.target.value;
+        setProductType(input);
+    }
+    function createProduct(){
+        
     }
     return(
         <>
         <NavbarSeller />
         <div>
-            <form className="myForm" action="http://localhost:5001/"method="POST">
+            <div className="myForm">
                 <label>Product Name</label>
-                <input name="name" placeholder="product name" />
+                <input onChange={handleName} name="name" placeholder="product name" />
                 <label>Price</label>
-                <input name="price" placeholder="product Prcie" />
-                <label>Product Type</label>
-                <select name="type">
-                    <option value="SUMMER_OUTFIT">Summer Outfit</option>
-                    <option value="WINTER_OUTFIT">Winter Outfit</option>
-                    <option value="GROCERIES">Groceries</option>
-                    <option value="HAND_TOOLS">Hand Tools</option>
-                    <option value="POWER_TOOLS">Power Tools</option>
-                </select>
-                <button className="btn btn-primary" type="submit">add</button>
-            </form>
+                <input onChange={handlePrice} name="price" placeholder="product Prcie" />
+                <label>Product Category</label>
+                <input onChange={handleCategory} name="name" placeholder="product category" />
+                <label>Product Category</label>
+                <input onChange={handleType} name="name" placeholder="product type" />
+                <button onClick={createProduct} className="btn btn-primary">add</button>
+            </div>
         </div>
         </>
     )
