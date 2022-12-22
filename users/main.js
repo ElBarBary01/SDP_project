d2c6b84309dcd41bad9d0d15d0187d7069c4f75b
Omar_ElBarbary <mohamedbassem576@gmail.com>
2022-12-22 22:13:16 +0200

    testing for classes

--- users/main.js
+++ users/main.js
@@ -1,22 +1,34 @@
 import Customer from "./Customer.js";
 import ProductSeller from "./ProductSeller.js";
+import CustomerService from "./CustomerService.js";
 import FactoryOrchestrator from "../factories/FactoryOrchestrator.js";
 
-var omar = new ProductSeller("omar","omar@gmail");
-//var omar2 = new Customer("omar","omar@gmail");
-
+/*
+SELLER TESTS
+var seller = new ProductSeller("Moody","Bassem@gmail");
 var product = await omar.addProduct({category:"GROCERIES",type:"EGG",name:"sbace test3",})
 console.log(product);
 //omar.checkProductPerformance(product.ssid);
 //omar.updateProduct({ssid:product.ssid,name:"sabce test2"});
-
+*/
 
 /*
+//CUSTOMER TESTS
 const groceriesFactory = new FactoryOrchestrator().createFactory("GROCERIES")
 const egg = groceriesFactory.createProduct("EGG");
 const meat = groceriesFactory.createProduct("MEAT");
-omar2.addToCart(egg)
-omar2.addToCart(meat)
+var customer = new Customer("omar","omar@gmail");
+customer.addToCart(egg)
+customer.addToCart(meat)
+customer.customersevice("Delivries are late")
 console.log(omar2.cart)
+*/
 
+/*
+ //CUSTOMER SERVICE TESTS
+
+ var service = new CustomerService("Youssef","Sbace@gmail.com")
+ var complaints =service.reviewComplain()
+ console.log(complaints)
+ service.reviewComplain({email:"omar@gmail",response:"we are working on it"})
 */
