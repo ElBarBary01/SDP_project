f7506027bc3250ea60c2d96697d0de92bd4b77a7
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2022-12-22 16:52:32 +0200

    test

--- factories/Main.js
+++ factories/Main.js
@@ -2,6 +2,6 @@ import FactoryOrchestrator from "./FactoryOrchestrator.js";
 
 
 const groceriesFactory = new FactoryOrchestrator().createFactory("GROCERIES")
-const egg = groceriesFactory.createProduct("EGG");
-console.log(egg.price);
+const egg = groceriesFactory.createProduct("MEAT");
+console.log(egg);
 
